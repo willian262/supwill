ba3c8a910e52bce14b6c113f15594c06002bd5a4
willian262 <willianmatias262@hotmail.com>
2026-04-14 10:37:04 -0300

    Update zoho.js

--- api/zoho.js
+++ api/zoho.js
@@ -91,7 +91,7 @@ export default async function handler(req, res) {
         const results = await Promise.all(
           froms.map(f => zohoRequest('tickets', {
             from: f, limit: LIMIT,
-            fields: 'id,ticketNumber,subject,status,statusType,departmentId,createdTime,modifiedTime,classification'
+            fields: 'id,ticketNumber,subject,status,statusType,departmentId,createdTime,modifiedTime,classification,assigneeId'
           }, accessToken))
         );
         let gotAny = false;
